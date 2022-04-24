ae6b8ccb0157bd74a9875ebccbce5d032c929350
Hero Fiennes-Tiffin <hero-fiennes-tiffin@MacBook-Air-Kirill.local>
2022-04-25 02:03:04 +0300

    first commit

---
+++ terminalReDesigne/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  terminalReDesigne
+//
+//  Created by Hero Fiennes-Tiffin on 25.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var terminalText: String = ""
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                HStack {
+                    Text("Проверка")
+                        .fontWeight(.light)
+                        .padding(.trailing, 700)
+                }
+                .frame(width: 800, height: 35)
+                Divider()
+                HStack {
+                    TextField(text: $terminalText)
+                }
+                .frame(width: 800, height: 600)
+                .background(Color.brown)
+            }
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+}
---
+++ terminalReDesigne/terminalReDesigneApp.swift
@@ -0,0 +1,17 @@
+//
+//  terminalReDesigneApp.swift
+//  terminalReDesigne
+//
+//  Created by Hero Fiennes-Tiffin on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct terminalReDesigneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
